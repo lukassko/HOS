54d150f88f254510e7fcfdc51d1a2681c077d8e7
Lukasz <lukasz.szczepanski.89@gmail.com>
2016-12-05 18:56:05 +0100

    tcp server

--- com.app.hos/src/main/java/com/app/hos/service/server/GatewayComm.java
+++
@@ -1,7 +0,0 @@
-package com.app.hos.service.server;
-
-public interface GatewayComm {
-
-	public void sendCommand (String msg);
-
-}
---
+++ com.app.hos/src/main/java/com/app/hos/service/server/Server.java
@@ -0,0 +1,10 @@
+package com.app.hos.service.server;
+
+public class Server {
+	
+	
+
+	public void serverTest(String input) {
+		System.out.println("Input: " + input);
+	}
+}
