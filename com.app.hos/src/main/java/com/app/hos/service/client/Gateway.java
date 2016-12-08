38074c9cba5dac498fd122c82cdc78247131756a
LS <ls@antsolutions.pl>
2016-12-08 16:41:13 +0100

    client side

---
+++ com.app.hos/src/main/java/com/app/hos/service/client/Gateway.java
@@ -0,0 +1,7 @@
+package com.app.hos.service.client;
+
+public interface Gateway {
+	
+	public String send(String text);
+
+}
---
+++ com.app.hos/src/main/java/com/app/hos/service/server/Deserializer.java
@@ -0,0 +1,15 @@
+package com.app.hos.service.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.springframework.core.serializer.Serializer;
+
+import com.app.hos.model.Command;
+
+public class Deserializer {
+
+
+
+}
