a4e531f2af0abae780267d1076a60a4577de6379
LS <ls@antsolutions.pl>
2016-12-09 09:49:59 +0100

    tcp event

--- com.app.hos/src/main/java/com/app/hos/service/integration/Event.java
+++ com.app.hos/src/main/java/com/app/hos/service/integration/Event.java
@@ -1,8 +1,10 @@
 package com.app.hos.service.integration;
 
+import org.springframework.integration.ip.tcp.connection.TcpConnectionEvent;
+
 public class Event {
 
-	public void eventTest () {
-		
+	public void eventTest (TcpConnectionEvent event) {
+		System.out.println("EVENT OCCURE!");
 	}
 }
